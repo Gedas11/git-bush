5df6263da7d1c2d7d35b3b706f634b6bd0d53f12
Gedas11 <97019328+Gedas11@users.noreply.github.com>
2022-04-05 21:37:53 +0200

    create.member

--- crud-task/src/App.jsx
+++ crud-task/src/App.jsx
@@ -2,14 +2,22 @@ import Table from "./components/Table";
 import RowList from "./components/RowList";
 import CreateForm from "./components/CreateForm";
 import Modal from "./components/Modal";
+import {useState} from 'react'
+import data from "./data/data.json";
 
 function App() {
+  const [crudData, setcrudData]= useState(data)
+  const onSave = (form) =>{
+    const copyData= {...crudData}
+    copyData.members.push(form)
+    setcrudData(copyData)
+  }
   return (
     <div className="jumbotron text-center">
-      <Table>
-        <RowList />
+      <Table data={crudData}>
+        <RowList data={crudData}/>
       </Table>
-      <CreateForm />
+      <CreateForm save={onSave}/>
       <Modal />
     </div>
   );
